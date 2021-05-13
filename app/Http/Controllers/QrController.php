ac66dc867abd9882fe2cad75b29df320dacaf09e
undertaker18 <jordanearlpascua28@gmail.com>
2021-05-13 11:00:43 +0800

    0

--- app/Http/Controllers/QrController.php
+++ app/Http/Controllers/QrController.php
@@ -18,48 +18,9 @@ class QrController extends Controller
         $title = 'Mr.';
         $email = 'john.doe@example.com';
 
-        // Addresses
-        $homeAddress = [
-            'type' => 'home',
-            'pref' => true,
-            'street' => '123 my street st',
-            'city' => 'My Beautiful Town',
-            'state' => 'LV',
-            'country' => 'Neverland',
-            'zip' => '12345-678',
-        ];
-        $wordAddress = [
-            'type' => 'work',
-            'pref' => false,
-            'street' => '123 my work street st',
-            'city' => 'My Dreadful Town',
-            'state' => 'LV',
-            'country' => 'Hell',
-            'zip' => '12345-678',
-        ];
+       
 
-        $addresses = [$homeAddress, $wordAddress];
-
-        // Phones
-        $workPhone = [
-            'type' => 'work',
-            'number' => '001 555-1234',
-            'cellPhone' => false,
-        ];
-        $homePhone = [
-            'type' => 'home',
-            'number' => '001 555-4321',
-            'cellPhone' => false,
-        ];
-        $cellPhone = [
-            'type' => 'work',
-            'number' => '001 9999-8888',
-            'cellPhone' => true,
-        ];
-
-        $phones = [$workPhone, $homePhone, $cellPhone];
-
-        return \QRCode::vCard($firstName, $lastName, $title, $email, $addresses, $phones)
+        return \QRCode::vCard($firstName, $lastName, $title, $email)
             ->setErrorCorrectionLevel('H')
             ->setSize(4)
             ->setMargin(2)
